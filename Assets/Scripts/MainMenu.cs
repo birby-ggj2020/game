e28304d929b2a9b7afc78fa19266fc068cfb8826
Phil Marsh <ajacobs@pc.factset.com>
2020-02-01 13:58:39 -0500

    - Fix game window size - Start mocking up biome selection

--- Assets/Scripts/MainMenu.cs
+++ Assets/Scripts/MainMenu.cs
@@ -5,11 +5,13 @@ using UnityEngine.SceneManagement;
 
 public class MainMenu : MonoBehaviour
 {
-    public void Play(){
-            SceneManager.LoadScene(1);
-    } 
+    public void Play()
+    {
+        SceneManager.LoadScene("BiomeSelector");
+    }
 
-    public void Exit(){
+    public void Exit()
+    {
         Application.Quit();
     }
 }
