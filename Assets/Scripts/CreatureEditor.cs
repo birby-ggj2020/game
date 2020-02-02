f805ac34b907703c77322b2188ca9e98b3e02e0c
Paul Sadauskas <psadauskas@factset.com>
2020-02-02 10:22:45 -0500

    Added audio for different scenes

--- Assets/Scripts/CreatureEditor.cs
+++ Assets/Scripts/CreatureEditor.cs
@@ -7,6 +7,12 @@ using UnityEditor;
 
 public class CreatureEditor : MonoBehaviour
 {
+
+    public AudioClip Grasslands;
+    public AudioClip Tundra;
+
+    public AudioClip Shore;
+    public AudioClip Desert;
     void Start()
     {
         HabitatState habitat_state = GameState.active_habitat_state;
@@ -20,6 +26,26 @@ public class CreatureEditor : MonoBehaviour
 
         var image = this.GetComponentInChildren<Image>();
         image.sprite = GameUtils.parse_sprite_path(habitat.background_img_path);
+
+        var audio =  Camera.main.GetComponent<AudioSource>();
+        switch(habitat.name){
+            case "Grasslands":
+                audio.clip = Grasslands;
+                break;
+            case "Tundra":
+                audio.clip = Tundra;
+                break;
+            case "Shore":
+                audio.clip = Shore;
+                break;
+            case "Desert":
+                audio.clip = Desert;
+                break;            
+            default:
+                break;
+        }
+        audio.loop = true;
+        audio.Play();
     }
 
     public void Back()
