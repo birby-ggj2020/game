fc04e42ef0c6e9a17d67a12adaddc76bc9441217
Phil Marsh <ajacobs@pc.factset.com>
2020-02-01 20:34:22 -0500

    Some cleanup: - Add GameState.active_habitat_state() helper - Remove old Debug logging - s/_CreaturePartsLoader/GameUtils/g

--- Assets/Scripts/Background.cs
+++ Assets/Scripts/Background.cs
@@ -9,22 +9,8 @@ public class Background : MonoBehaviour
     void Start()
     {   if(GameState.active_habitat_index.HasValue){
             var image = this.GetComponent<Image>();
-            var path = GameState.habitats[GameState.active_habitat_index.Value].habitat.background_img_path;
-            image.sprite = _parse_sprite_path(path);
+            var path = GameState.active_habitat_state().habitat.background_img_path;
+            image.sprite = GameUtils.parse_sprite_path(path);
         }
-    }
-
-    public static Sprite _parse_sprite_path(string sprite_path)
-    {
-        var texture = AssetDatabase.LoadAssetAtPath(
-            sprite_path,
-            typeof(Texture2D)
-        ) as Texture2D;
-        var sprite = Sprite.Create(
-            texture,
-            new Rect(0, 0, texture.width, texture.height),
-            new Vector2(0f, 0f), 1.0f
-        );
-        return sprite;       
     }
 }
